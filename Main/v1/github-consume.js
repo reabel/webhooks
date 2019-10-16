805b0491357cb90135c6d3b53d3c041fdf1ff4f2
Ryan Reabel <rreabel@i-sight.com>
2019-10-16 00:19:04 -0400

    updates to github-consume to allow for proper parsing of json body

--- Main/v1/github-consume.js
+++ Main/v1/github-consume.js
@@ -26,24 +26,30 @@ testRun();*/
 /* general express server.*/
 
 //const app = express();
-const express = require('express')
-const app = express()
-const port = 4567
-
-app.get('/', (req, res) => res.send('Hello World! TEST'));
-app.post('/payload', (req, res) => {
-	processRespFirst(res);
-	console.log('waiting 10 seconds pretending like we\'re doing stuff');
-	setTimeout(function(){ console.log('Request:', req.body);
-	 }, 10000);
-	//
-	//res.send('');
-});
+const express = require('express');
+const app = express();
+const port = 4567;
 
 
 function processRespFirst(res){
 	res.status(204);
 	res.send('Worked');
 }
+
+function postRespProcess(req){
+	console.log('waiting 10 seconds pretending like we\'re doing stuff');
+	//body not showing up for some reason
+	setTimeout(function(){ console.log('Request:', req);
+	 }, 10000);
+}
+app.use(express.json());
+app.post('/payload', (req, res) => {
+	processRespFirst(res);
+	postRespProcess(req);
+	//
+	//res.send('');
+});
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
